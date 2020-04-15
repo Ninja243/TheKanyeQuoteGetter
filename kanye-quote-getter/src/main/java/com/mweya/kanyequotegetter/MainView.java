2c0b36f62b20342f37c960a5f4ac29d0c36c89bf
Ninja243 <mweyaruider@gmail.com>
2020-04-16 00:47:51 +0200

    Centered stuff

--- kanye-quote-getter/src/main/java/com/mweya/kanyequotegetter/MainView.java
+++ kanye-quote-getter/src/main/java/com/mweya/kanyequotegetter/MainView.java
@@ -1,11 +1,17 @@
 package com.mweya.kanyequotegetter;
 
+import javax.swing.GroupLayout.Alignment;
+
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import org.springframework.web.client.RestTemplate;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.panel.Panel;
 
 @Route
 public class MainView extends VerticalLayout {
@@ -13,12 +19,16 @@ public class MainView extends VerticalLayout {
     private static final long serialVersionUID = 1L;
 
     // Elements to add to the view later
-    Label text = new Label("This is the Kanye quote getter");
+    H1 text = new H1("This is the Kanye quote getter");
     Button button = new Button("Get a quote", event -> callRestService());
 
     public MainView() {
-        add(text);
-        add(button);
+        // Styling
+        HorizontalLayout h = new HorizontalLayout();
+        h.setVerticalComponentAlignment(Alignment.CENTER, text);
+        h.setVerticalComponentAlignment(Alignment.CENTER, button);
+        // Actually add things to the page
+        add(text, button);
     }
 
     /**
@@ -27,7 +37,14 @@ public class MainView extends VerticalLayout {
     private void callRestService() {
         RestTemplate restTemplate = new RestTemplate();
         Quote result = restTemplate.getForObject("https://api.kanye.rest/", Quote.class);
-        Notification.show(result.toString());
+        Dialog dialog = new Dialog();
+        VerticalLayout layout = new VerticalLayout();
+        Button close = new Button("Close", event -> dialog.close());
+        // Notification.show(result.toString());
+        layout.add(new Label(result.toString()));
+        layout.add(close);
+        dialog.add(layout);
+        dialog.open();
     }
 
 }
